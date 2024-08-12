721f42811ac8364a13e23e6f5d9f11e895fa8a1e
DongGyu Kim <57940897+kimdonggyu2008@users.noreply.github.com>
2024-08-12 21:06:55 +0900

    Update model.py

--- pflow/utils/model.py
+++ pflow/utils/model.py
@@ -7,6 +7,8 @@ import torch
 def sequence_mask(length, max_length=None):
     #if max_length is None:
     max_length = length.max()
+    for leng in length:
+        print(leng)
     x = torch.arange(max_length, dtype=length.dtype, device=length.device)
     return x.unsqueeze(0) < length.unsqueeze(1)
 
